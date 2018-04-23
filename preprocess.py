2dab385002a5342b90686d66fb8a6cf9af952acf
Young Wang <chenyang.wang123@gmail.com>
2018-04-23 14:42:06 -0500

    refactor

--- preprocess.py
+++ preprocess.py
@@ -5,22 +5,26 @@ QNLS_PER_PHRASE = 4
 TOKENS_PER_QNL = 8
 NOTE_RANGE = 128
 
-class ObjectsToTokens(object):
+class ObjectIndex(object):
 	def __init__(self, maximum=-1):
-		self.items = []
+		self.objs = []
 		self.max = maximum
 
-	def get(self, item):
-		if item not in self.items:
-			if len(self.items) == self.max:
+	def getIndex(self, obj):
+		if obj not in self.objs:
+			if len(self.objs) == self.max:
 				raise IndexError('maximum value reached')
-			self.items.append(item)
+			self.objs.append(obj)
 
-		return self.items.index(item)
+		return self.objs.index(obj)
 
-COMPOSERS = ObjectsToTokens(30)
-TIME_SIGS = ObjectsToTokens(8)
-KEYS = ObjectsToTokens(13)
+	def getObject(self, i):
+		return self.objs[i]
+
+
+Composers = ObjectIndex(30)
+TimeSignatures = ObjectIndex(8)
+KeySignatures = ObjectIndex(13)
 
 
 class IntervalSet(object):
@@ -50,46 +54,47 @@ class IntervalSet(object):
 
 class MusicPiece(object):
 	def __init__(self, composer, path):
+		self.notes = IntervalSet()
 		self.tempos = IntervalSet()
 		self.time_sigs = IntervalSet()
 		self.keys = IntervalSet()
-		self.notes = IntervalSet()
-		self.composer = COMPOSERS.get(composer)
+		self.composer = Composers.getIndex(composer)
 		self.qnls = 0
 
 		# Default values for metadata
 		self.tempos.addPosEdge('tp', 0, 120)
-		self.time_sigs.addPosEdge('ts', 0, TIME_SIGS.get((4, 4)))
-		self.keys.addPosEdge('key', 0, KEYS.get('unknown'))
+		self.time_sigs.addPosEdge('ts', 0, TimeSignatures.getIndex((4, 4)))
+		self.keys.addPosEdge('key', 0, KeySignatures.getIndex('unknown'))
 
 		mid = mido.MidiFile(path)
 		for track in mid.tracks:
 			time = 0 # In qnls
 			for msg in track:
 				time += float(msg.time) / mid.ticks_per_beat
-				if msg.type == 'time_signature':
-					self.time_sigs.addNegEdge('ts', time)
-					self.time_sigs.addPosEdge('ts', time, TIME_SIGS.get((msg.numerator, msg.denominator)))
-				elif msg.type == 'key_signature':
-					self.keys.addNegEdge('key', time)
-					self.keys.addPosEdge('key', time, KEYS.get(msg.key))
-				elif msg.type == 'set_tempo':
-					self.tempos.addNegEdge('tp', time)
-					self.tempos.addPosEdge('tp', time, mido.tempo2bpm(msg.tempo))
-				elif msg.type == 'note_on':
+				if msg.type == 'note_on':
 					self.notes.addPosEdge(msg.note, time, msg.note)
 				elif msg.type == 'note_off':
 					self.notes.addNegEdge(msg.note, time)
+				elif msg.type == 'set_tempo':
+					self.tempos.addNegEdge('tp', time)
+					self.tempos.addPosEdge('tp', time, mido.tempo2bpm(msg.tempo))
+				elif msg.type == 'time_signature':
+					self.time_sigs.addNegEdge('ts', time)
+					self.time_sigs.addPosEdge('ts', time, TimeSignatures.getIndex((msg.numerator, msg.denominator)))
+				elif msg.type == 'key_signature':
+					self.keys.addNegEdge('key', time)
+					self.keys.addPosEdge('key', time, KeySignatures.getIndex(msg.key))
 
 			self.qnls = max(self.qnls, time)
 
+		self.notes.finalize(self.qnls)
 		self.tempos.finalize(self.qnls)
 		self.time_sigs.finalize(self.qnls)
 		self.keys.finalize(self.qnls)
-		self.notes.finalize(self.qnls)
 
 	def getTrainingExamples(self, qnls=QNLS_PER_PHRASE, gran=TOKENS_PER_QNL):
-		return [self.featureSeq(s, qnls, gran) for s in np.arange(0, self.qnls, qnls) 
+		label = self.labelVec()
+		return [(self.featureSeq(s, qnls, gran), label) for s in np.arange(0, self.qnls, qnls) 
 				if s + qnls <= self.qnls]
 
 	def featureSeq(self, start, qnls=QNLS_PER_PHRASE, gran=TOKENS_PER_QNL):
@@ -101,14 +106,16 @@ class MusicPiece(object):
 
 		tempo = self.tempos.getValuesAt(t)[0]
 
-		time_sig_vec = np.zeros(TIME_SIGS.max)
+		time_sig_vec = np.zeros(TimeSignatures.max)
 		time_sig_vec[self.time_sigs.getValuesAt(t)] = 1
 
-		key_vec = np.zeros(KEYS.max)
+		key_vec = np.zeros(KeySignatures.max)
 		key_vec[self.keys.getValuesAt(t)] = 1
 
-		composer_vec = np.zeros(COMPOSERS.max)
-		composer_vec[self.composer] = 1
+		return np.r_[notes_vec, tempo, time_sig_vec, key_vec]
 
-		return np.r_[notes_vec, tempo, time_sig_vec, key_vec, composer_vec]
+	def labelVec(self):
+		composer_vec = np.zeros(Composers.max)
+		composer_vec[self.composer] = 1
+		return composer_vec
 
