0c9a74d42e8086b1da1610a79fa98d92560c7ba2
sepideh maleki <smaleki@cs.utexas.edu>
2018-04-24 19:26:50 -0500

    adding composers

--- dl.py
+++ dl.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
+import os
 import webbrowser
 import urllib2
 import urlparse
 import requests
-from time import sleep
+from collections import defaultdict
 import re
 import wget
-urlList = []
+urlList = defaultdict(list)
 composers = ["Adam", "Alkan", "J.S. Bach", "Banchieri", "Beethoven", "Billings", "Bossi", "Brahms", "Buxtehude", "Byrd", "Chopin", "Clementi", "Corelli", "Dufay", "Dunstable", "Field", "Flecha", 
         "Foster", "Frescobaldi", "Gershwin", "Giovannelli", "Grieg", "Haydn", "Himmel", "Hummel", "Isaac", "Ives", "Joplin", "Josquin", "Landini", "Lassus", "Liszt", "MacDowell", "Mendelssohn",
         "Monteverdi", "Mozart", "Pachelbel", "Prokofiev", "Ravel", "Scarlatti", "Schubert", "Schumann", "Scriabin", "Sinding", "Sousa", "Turpin", "Scarlatti", "Vecchi", "Victoria", "Vivaldi", "Weber"]
@@ -20,21 +21,25 @@ def getAllUrl(url):
         soup.prettify()
         for composer in composers:
             for anchor in soup.findAll('a', href = re.compile(composer)):
-                urlList.append(anchor['href'])
+                urlList[composer].append(anchor['href'])
     except urllib2.HTTPError, e:
         print(e)
 def downloading_midi(urlList):
-    for l in urlList:
-        print(l)
-        r = requests.get(l)
-        soup = BeautifulSoup(r.content, 'lxml')
-        for anchor in soup.findAll('a', href=True):
-            if "midi" in anchor['href']:
-                midi_link = anchor['href']
-                try:
-                    wget.download(midi_link, '/u/smaleki/workspace/nlp/final')
-                except:
-                    print("cant download")
+    for composer in urlList:
+        listUrl = urlList[composer]
+        newpath = "/u/smaleki/workspace/nlp/final/" + composer
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+        for l in listUrl:
+            r = requests.get(l)
+            soup = BeautifulSoup(r.content, 'lxml')
+            for anchor in soup.findAll('a', href=True):
+                if "midi" in anchor['href']:
+                    midi_link = anchor['href']
+                    try:
+                        wget.download(midi_link, "/u/smaleki/workspace/nlp/final/" + composer)
+                    except:
+                        print("cant download")
 
 if __name__ == "__main__":
     urls = getAllUrl('http://kern.ccarh.org/')
